91b0208228eba581bc807adffb6defd3c8627710
Stijn Slebos <stijnslebos@gmail.com>
2020-07-28 12:59:51 +0100

    Made screen correct aspect ratio

--- framework.js
+++ framework.js
@@ -13,7 +13,6 @@ const matrix = new LedMatrix(
 		cols: 64,
 		chainLength: 1,
 		hardwareMapping: GpioMapping.Regular,
-		parallel: 3,
 		pixelMapperConfig: LedMatrixUtils.encodeMappers({
 			type: PixelMapperType.ChainLink
 		})
@@ -27,12 +26,28 @@ const matrix = new LedMatrix(
 console.log('Intialised matrix');
 
 matrix.afterSync((mat, dt, t) => {
-	matrix.clear().brightness(10).fgColor(colors[matrixColor]).fill();
+//	matrix.clear().brightness(10).fgColor(colors[matrixColor]).fill();
+matrix
+  .clear()            // clear the display
+  .brightness(50)    // set the panel brightness to 100%
+  .fgColor(0x0000FF)  // set the active color to blue
+  .fill()             // color the entire diplay blue
+  .fgColor(0xFFFF00)  // set the active color to yellow
+  // draw a yellow circle around the display
+  .drawCircle(32, 32, 31)
+  // draw a yellow rectangle
+  .drawRect(16, 16, 32, 32)
+  // sets the active color to red
+  .fgColor({ r: 255, g: 0, b: 0 })
+  // draw two diagonal red lines connecting the corners
+  .drawLine(0, 0, matrix.width(), matrix.height())
+  .drawLine(matrix.width() - 1, 0, 0, matrix.height() - 1);
 	setTimeout(() => matrix.sync(), 0);
 });
 
 matrix.sync();
-
+console.log("Width = " + matrix.width());
+console.log("Height = " + matrix.height());
 // Initialise buttons
 console.log('Intialising buttons')
 rpio.open(32, rpio.INPUT, rpio.PULL_UP);
