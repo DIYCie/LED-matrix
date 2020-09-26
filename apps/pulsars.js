7923b6767d1551ed0ffda5a961929217afb24b34
Ruben Smit <RubenSmit@users.noreply.github.com>
2020-09-26 20:01:31 +0200

    Optimize code for speed

--- apps/pulsars.js
+++ apps/pulsars.js
@@ -1,33 +1,16 @@
 import MatrixApplication from '../MatrixApplication';
 import Button from "../Button";
 
-class Pulsar {
-    constructor(x, y, f) {
-        this.x = x;
-        this.y = y;
-        this.f = f;
-    }
-
-    nextColor(time, color) {
-        const brightness = 0xFF & Math.max(0, 255 * (Math.sin(this.f * time)));
-
-        return color & ( (brightness << 16) | (brightness << 8) | brightness );
-    }
-}
-
 module.exports = class Pulsars extends MatrixApplication {
     static get name() { return 'Pulsars'; }
     static get description() { return 'Displays dots that randomly pulse. Color, speed and size can be controlled with te buttons.'; }
 
     constructor(matrix) {
         super(matrix);
-        this.pulsars = [];
-        this.size = 1;
-        this.speed = 10000;
+        this.size = 8;
+        this.speed = 10;
         this.colors = [0xFFFFFF, 0xFF00000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF];
         this.color = 0;
-
-        this.createPulsars(matrix);
     }
 
     setup() {
@@ -36,35 +19,30 @@ module.exports = class Pulsars extends MatrixApplication {
 
     draw(deltaTime, time) {
         super.draw();
-        this.pulsars.forEach(pulsar => {
-            let color = pulsar.nextColor(time/this.speed, this.colors[this.color]);
-            for (let i = 0; i < this.size; i++) {
-                for (let j = 0; j < this.size; j++) {
-                    this.matrix.fgColor(color).setPixel(pulsar.x + i, pulsar.y + j);
-                }
+        const timestep = time/100000*this.speed;
+        for (let x = 0; x < this.matrix.width()/this.size; x++) {
+            for (let y = 0; y < this.matrix.height()/this.size; y++) {
+                this.matrix.fgColor(this.pixelColor(timestep, x, y, this.colors[this.color]))
+                  .fill(x*this.size, y*this.size, x*this.size + this.size, y*this.size + this.size);
             }
-        });
+        }
     }
 
     onButtonPressed(button) {
-        if(button === Button.UP && this.size < 8) {
+        if(button === Button.UP && this.size < 16) {
             this.size++;
-            this.pulsars = [];
-            this.createPulsars(this.matrix);
-        } else if(button === Button.DOWN && this.size > 1) {
+        } else if(button === Button.DOWN && this.size > 2) {
             this.size--;
-            this.pulsars = [];
-            this.createPulsars(this.matrix);
-        } else if(button === Button.LEFT && this.speed < 100000) {
-            this.speed += 1000;
-        } else if(button === Button.RIGHT && this.speed > 1000) {
-            this.speed -= 1000;
+        } else if(button === Button.LEFT && this.speed < 100) {
+            this.speed++;
+        } else if(button === Button.RIGHT && this.speed > 1) {
+            this.speed--;
         } else if(button === Button.A) {
-            if (this.color++ > this.colors.length-1) {
+            if (this.color++ >= this.colors.length-1) {
                 this.color = 0;
             }
         } else if(button === Button.B) {
-            if (this.color-- < 0) {
+            if (this.color-- <= 0) {
                 this.color = this.colors.length-1;
             }
         }
@@ -73,11 +51,10 @@ module.exports = class Pulsars extends MatrixApplication {
     onButtonReleased(button) {
     }
 
-    createPulsars(matrix) {
-        for (let x = 0; x < matrix.width()/this.size; x++) {
-            for (let y = 0; y < matrix.height()/this.size; y++) {
-                this.pulsars.push(new Pulsar(x*this.size, y*this.size, 5 * Math.random()));
-            }
-        }
+    pixelColor(time, x, y, color) {
+        const offset = (x*x*x*23)%61 + ((x+1)*y*y*43)%73
+        const brightness = 0xFF & Math.max(0, 255 * (Math.sin(((time + offset) * offset/5))));
+
+        return color & ( (brightness << 16) | (brightness << 8) | brightness );
     }
 }
